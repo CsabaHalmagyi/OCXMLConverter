27a6e1006d5810bf11c570325ee78fe80f9f3eb2
csaba <csaba@ebi.ac.uk>
2017-09-11 15:51:27 +0100

    Initial commit

---
+++ console-commands/src/main/java/com/horizonshards/ocxmlconverter/Application.java
@@ -0,0 +1,17 @@
+package com.horizonshards.ocxmlconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Created by csaba on 11/09/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
